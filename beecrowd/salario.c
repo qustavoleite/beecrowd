968a9bf8979d18854a86daadcddcd05a069623c2
qustavoleite <qustavoleite@gmail.com>
2023-08-26 22:34:48 -0300

    new exercises

--- beecrowd/salario.c
+++ beecrowd/salario.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
 
-int main() {
-  char nVendedor;
-  double sFixo, tVendas;
+int main () {
+    int numero, horasTrabalhadas;
+    double valorHora, salario;
+    scanf("%d", &numero);
+    scanf("%d", &horasTrabalhadas);
+    scanf("%lf", &valorHora);
 
-  scanf("%s", &nVendedor);
-  scanf("%lf %lf", &sFixo, &tVendas);
-  printf("TOTAL = R$ %.2f\n", (sFixo + tVendas * 0.15));
+    salario = horasTrabalhadas * valorHora;
+    printf("NUMBER = %d\n", numero);
+    printf("SALARY = U$ %.2lf\n", salario);
 
-  return 0;
+    return 0;
 }
-
-// #include <stdio.h>
-
-// int main() {
-//     char nVendedor[20];
-//     double sFixo, tVendas, total;
-
-//     scanf("%s %lf %lf", nVendedor, &sFixo, &tVendas);
-//     total = sFixo + tVendas * 0.15;
-//     printf("TOTAL = R$ %.2lf", total);
-
-//     return 0;
-// }
